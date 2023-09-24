c504d6e7671dcb2e7082f246842965c6bd52a71b
Travis C <140869835+TravMan3@users.noreply.github.com>
2023-09-24 16:24:06 -0600

    worked on

--- src/com/coderscampus/Main.java
+++ src/com/coderscampus/Main.java
@@ -1,9 +1,33 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		int randomNumber = random.nextInt(100) + 1;
+		System.out.println(randomNumber);
+		PickNumber pickedInput = new PickNumber();
 		
+		String answer = pickedInput.picNum(scanner,"Pick a number between 1 and 100");
+		Integer upgradedInput = Integer.parseInt(answer);
+		if (upgradedInput < 1 ) {
+			System.out.println("Please pick a higher number");
+			if (upgradedInput > 100) {
+				System.out.println("Please pick a lower number");
+			}
+		}else if (upgradedInput >= 1 && upgradedInput <= 100 && upgradedInput
+				!= randomNumber) {
+			System.out.println("You win");
+		}
+		System.out.println(upgradedInput);
+		
+		
+		
+		scanner.close();
 	}
-
+		
 }
---
+++ src/com/coderscampus/PickNumber.java
@@ -0,0 +1,14 @@
+package com.coderscampus;
+
+import java.util.Scanner;
+
+public class PickNumber {
+
+	String picNum (Scanner scanner, String displayedMessage) {
+		System.out.println(displayedMessage);
+		String userInput = scanner.nextLine();
+		return userInput;
+	}
+//	Integer upgradedInput = Integer.parseInt(userInput);
+//	return upgradedInput;
+}
