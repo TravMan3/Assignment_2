5ec1081e13182d5e99effa24365801fbab08028f
Travis C <140869835+TravMan3@users.noreply.github.com>
2023-09-25 00:19:42 -0600

    Getting closer

--- src/com/coderscampus/Main.java
+++ src/com/coderscampus/Main.java
@@ -10,12 +10,24 @@ public class Main {
 		Random random = new Random();
 		int randomNumber = random.nextInt(100) + 1;
 		System.out.println(randomNumber);
+		
 		PickNumber pickedInput = new PickNumber();
 		
-		String answer = pickedInput.picNum(scanner,"Pick a number between 1 and 100");
-		Integer upgradedInput = Integer.parseInt(answer);
+		//String answer = pickedInput.picNum(scanner,"Pick a number between 1 and 100");
 		
-		System.out.println(upgradedInput);
+		for(int i = 1; i <= 5; i++) {
+			String answer = pickedInput.picNum(scanner,"Pick a number between 1 and 100");
+			Integer newNum = pickedInput.reCreate(answer);
+			if (newNum == randomNumber) {	
+				System.out.println("\n" + "You win!");
+				break;
+			}
+			Integer betweenCheck = pickedInput.oneHunCheck(newNum);
+			Integer checkOne = pickedInput.checkNum(betweenCheck, randomNumber);
+//			System.out.println("\n" + "You lose!");
+//			System.out.println("\n" + "The number to guess was: " + randomNumber);
+		}
+		
 		
 		
 		
--- src/com/coderscampus/PickNumber.java
+++ src/com/coderscampus/PickNumber.java
@@ -5,10 +5,30 @@ import java.util.Scanner;
 public class PickNumber {
 
 	String picNum (Scanner scanner, String displayedMessage) {
-		System.out.println(displayedMessage);
+		System.out.print("\n" + displayedMessage + " ");
 		String userInput = scanner.nextLine();
 		return userInput;
 	}
-//	Integer upgradedInput = Integer.parseInt(userInput);
-//	return upgradedInput;
+	
+	Integer reCreate (String a) {
+		Integer upgradedInput = Integer.parseInt(a);
+		return upgradedInput;		
+	}
+	
+	Integer oneHunCheck (Integer check) {
+		if (check < 1 || check > 100) {
+			System.out.println("\n" + "Your guess is not between 1 and "
+					+ "100, please try again");			
+		}
+		return check;
+	}
+
+	Integer checkNum (Integer theNum, Integer random) {
+		if (theNum > random) {
+			System.out.println("\n" + "Please pick a lower number");
+		}else if (theNum < random) {
+			System.out.println("\n" + "Please pick a higher number");
+		}
+		return 9876;
+	}
 }
